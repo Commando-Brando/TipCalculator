31f7ab48b168a03cdd1a2b277ef5f9c5720bb1b5
Brandon Muller <branmuller@gmail.com>
2021-12-27 19:15:28 -0600

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Brandon Muller on 12/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
